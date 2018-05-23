f679eb512284cb591bca080c806e98cdc489901f
Valentyn Ponomarenko <bootloader@list.ru>
2018-05-23 09:52:46 -0500

    1. Update BlockChain with Transactions / Not spent outputs / Build Fix.

--- Transaction.go
+++ Transaction.go
@@ -62,3 +62,9 @@ func NewCoinbaseTX(to, data string) *Transaction {
 	return &tx
 }
 
+/*
+
+*/
+func (tx Transaction) IsCoinbase() bool  {
+	return len(tx.Vin) == 1 && len(tx.Vin[0].Txid) == 0 && tx.Vin[0].Vout == -1
+}
